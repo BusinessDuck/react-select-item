fcbb06aef927394f0c5768b62b7812c8cad1c0ff
Dmitriy Yurov <narkozzforb@gmail.com>
2017-04-22 19:30:45 +0300

    Search input beta release

--- example/example.js
+++ example/example.js
@@ -8,73 +8,73 @@ var option = React.createElement.bind(null, 'option')
 var h1 = React.createElement.bind(null, 'h1')
 
 var Example = React.createFactory(React.createClass({
-    displayName: 'Example',
-    getInitialState: function () {
-        return {
-            color: null,
-            colors: []
-        }
-    },
-    handleChange: function (color) {
-        this.setState({ color: color })
-    },
-    handleMultiChange: function (colors) {
-        this.setState({ colors: colors })
-    },
-    render: function () {
-        return (
-            div({ className: "example" },
-                h1(null, "Select Item Example"),
-                SelectBox(
-                    {
-                        label: "Favorite Color",
-                        className: 'my-example-select-box',
-                        onChange: this.handleChange,
-                        value: this.state.color
-                    },
-                    option({ key: 'red', value: 'red' }, 'Red'),
-                    option({ value: 'green' }, 'Green'),
-                    option({ value: 'blue' }, 'Blue'),
-                    option({ value: 'black' }, 'Black'),
-                    option({ value: 'orange' }, 'Orange'),
-                    option({ value: 'greenish' }, 'Light greenish with a little bit of yellow')
-                ),
-                h1(null, "Multi Select Example"),
-                SelectBox(
-                    {
-                        label: "Favorite Colors",
-                        onChange: this.handleMultiChange,
-                        value: this.state.colors,
-                        multiple: true
-                    },
-                    option({ value: 'red' }, 'Red'),
-                    option({ value: 'green' }, 'Green'),
-                    option({ value: 'blue' }, 'Blue'),
-                    option({ value: 'black' }, 'Black'),
-                    option({ value: 'orange' }, 'Orange'),
-                    option({ value: 'greenish' }, 'Light greenish with a little bit of yellow')
-                ),
-                h1(null, "Search Example"),
-                SelectBox(
-                    {
-                        label: "Favorite Colors",
-                        onChange: this.handleMultiChange,
-                        value: this.state.colors,
-                        multiple: true,
-                        filterFn: function (text, item) {
-                                return item.label.indexOf(text) !== -1
-                            },
+	displayName: 'Example',
+	getInitialState: function () {
+		return {
+			color: null,
+			colors: []
+		}
+	},
+	handleChange: function (color) {
+		this.setState({color: color})
+	},
+	handleMultiChange: function (colors) {
+		this.setState({colors: colors})
+	},
+	render: function () {
+		return (
+			div({className: "example"},
+				h1(null, "Select Item Example"),
+				SelectBox(
+					{
+						label: "Favorite Color",
+						className: 'my-example-select-box',
+						onChange: this.handleChange,
+						value: this.state.color
+					},
+					option({key: 'red', value: 'red'}, 'Red'),
+					option({value: 'green'}, 'Green'),
+					option({value: 'blue'}, 'Blue'),
+					option({value: 'black'}, 'Black'),
+					option({value: 'orange'}, 'Orange'),
+					option({value: 'greenish'}, 'Light greenish with a little bit of yellow')
+				),
+				h1(null, "Multi Select Example"),
+				SelectBox(
+					{
+						label: "Favorite Colors",
+						onChange: this.handleMultiChange,
+						value: this.state.colors,
+						multiple: true
+					},
+					option({value: 'red'}, 'Red'),
+					option({value: 'green'}, 'Green'),
+					option({value: 'blue'}, 'Blue'),
+					option({value: 'black'}, 'Black'),
+					option({value: 'orange'}, 'Orange'),
+					option({value: 'greenish'}, 'Light greenish with a little bit of yellow')
+				),
+				h1(null, "Search Example"),
+				SelectBox(
+					{
+						label: "Favorite Colors",
+						onChange: this.handleMultiChange,
+						value: this.state.colors,
+						multiple: true,
+						filterFn: function (text, item) {
+							return item.label.indexOf(text) !== -1
+						},
 
-                    },
-                    option({ value: 'red' }, 'Red'),
-                    option({ value: 'green' }, 'Green'),
-                    option({ value: 'blue' }, 'Blue'),
-                    option({ value: 'black' }, 'Black'),
-                    option({ value: 'orange' }, 'Orange'),
-                    option({ value: 'greenish' }, 'Light greenish with a little bit of yellow')
-                )
-            ))
-    }
+					},
+					option({value: 'red'}, 'Red'),
+					option({value: 'green'}, 'Green'),
+					option({value: 'blue'}, 'Blue'),
+					option({value: 'black'}, 'Black'),
+					option({value: 'orange'}, 'Orange'),
+					option({value: 'greenish'}, 'Light greenish with a little bit of yellow')
+				)
+			))
+	}
 }))
 
 document.body.appendChild(container)
